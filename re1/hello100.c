9e90a5068f7501cef067628d555c72518d77c550
meitel <u461566g@ecs.osaka-u.ac.jp>
2018-04-16 14:05:13 +0900

    全課題のlogを取った

--- re1/hello100.c
+++ re1/hello100.c
@@ -5,12 +5,12 @@
 int main(int ac, char* av[]){
 	int i;
 	// [ 下記 2 行の内、いずれかをコメントアウトする ]
-	char message[] = "hello";
-	// char message[] = "hello\n";
+	//char message[] = "hello";
+	char message[] = "hello\n";
 	for(i = 0; i < COUNT; i ++){
 		// [ 下記 2 行の内、いずれかをコメントアウトする ]
 		fwrite(message, strlen(message), 1, stdout);
-		// write(0, message, strlen(message));
+		//write(0, message, strlen(message));
 	}
 	return 0;
 }
