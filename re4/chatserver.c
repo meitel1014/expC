47c296379d7c277b5f2ac5329258fe2c05478127
meitel1014 <meitel1014@gmail.com>
2018-07-05 16:38:58 +0900

    サーバ機能追加

--- re4/chatserver.c
+++ re4/chatserver.c
@@ -1,5 +1,7 @@
+#include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,14 +15,15 @@
 typedef struct socklist {
 	int csock;
 	char username[101];
-	struct socklist *next;
+	struct socklist* next;
 } SOCKLIST;
 
-SOCKLIST *sockhead = NULL;
+SOCKLIST* sockhead = NULL;
 int csocknum = 0;
+int time_up = 0;
 
-SOCKLIST *add_sock(int csock) {
-	SOCKLIST *new = malloc(sizeof(SOCKLIST));
+SOCKLIST* add_sock(int csock) {
+	SOCKLIST* new = malloc(sizeof(SOCKLIST));
 	new->csock = csock;
 	new->next = sockhead;
 	sockhead = new;
@@ -29,8 +32,8 @@ SOCKLIST *add_sock(int csock) {
 }
 
 int remove_sock(int csock) {
-	SOCKLIST *tmp = sockhead;
-	SOCKLIST *prev = NULL;
+	SOCKLIST* tmp = sockhead;
+	SOCKLIST* prev = NULL;
 	while(tmp != NULL) {
 		if(tmp->csock == csock) {
 			if(prev != NULL) {
@@ -48,10 +51,21 @@ int remove_sock(int csock) {
 	return -1;
 }
 
-int main(int argc, char *argv[]) {
+void broadcast(char* msg) {
+	SOCKLIST* bsock = sockhead;
+	while(bsock != NULL) {
+		write(bsock->csock, msg, strlen(msg));
+		bsock = bsock->next;
+	}
+}
+
+void timeup(int sig) {
+	time_up = 1;
+}
+
+int main(int argc, char* argv[]) {
 	int sock;
-	struct sockaddr_in svr;
-	struct sockaddr_in clt;
+	struct sockaddr_in svr, clt;
 	char rbuf[1024];
 	int nbytes;
 
@@ -74,7 +88,7 @@ int main(int argc, char *argv[]) {
 	/* 受付側の IP アドレスは任意 */
 	svr.sin_port = htons(PORT); /* ポート番号 10140 を介して受け付ける */
 	/* ソケットにソケットアドレスを割り当てる */
-	if(bind(sock, (struct sockaddr *)&svr, sizeof(svr)) < 0) {
+	if(bind(sock, (struct sockaddr*)&svr, sizeof(svr)) < 0) {
 		perror("bind");
 		exit(1);
 	}
@@ -84,6 +98,8 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
+	signal(SIGINT, SIG_IGN);
+
 	for(;;) {
 		// s2 (入力待ち)
 		int maxfd = sock;
@@ -91,7 +107,7 @@ int main(int argc, char *argv[]) {
 		/* 入力を監視するファイル記述子の集合を変数 rfds にセットする */
 		FD_ZERO(&rfds);      /* rfds を空集合に初期化 */
 		FD_SET(sock, &rfds); /* クライアントを受け付けるソケット */
-		SOCKLIST *tmp = sockhead;
+		SOCKLIST* tmp = sockhead;
 		while(tmp != NULL) {
 			FD_SET(tmp->csock, &rfds); /* クライアントを受け付けたソケット */
 			if(tmp->csock > maxfd) {
@@ -107,7 +123,7 @@ int main(int argc, char *argv[]) {
 				/* クライアントの受付 */
 				int csock;
 				socklen_t clen = sizeof(clt);
-				if((csock = accept(sock, (struct sockaddr *)&clt, &clen)) < 0) {
+				if((csock = accept(sock, (struct sockaddr*)&clt, &clen)) < 0) {
 					perror("accept");
 					exit(1);
 				}
@@ -118,11 +134,11 @@ int main(int argc, char *argv[]) {
 				}
 
 				write(csock, "REQUEST ACCEPTED\n", 17);
-				SOCKLIST *newsock = add_sock(csock);
+				SOCKLIST* newsock = add_sock(csock);
 				// s5 (ユーザ名登録)
 				nbytes = read(csock, rbuf, sizeof(rbuf));
 				rbuf[nbytes - 1] = '\0';
-				SOCKLIST *tmp = sockhead;
+				SOCKLIST* tmp = sockhead;
 				while(tmp != NULL) {
 					if(strcmp(tmp->username, rbuf) == 0) {
 						write(csock, "USERNAME REJECTED\n", 17);
@@ -139,32 +155,75 @@ int main(int argc, char *argv[]) {
 				continue;
 			}
 			// s6 (メッセージ配信)
-			SOCKLIST *msgsock = sockhead;
+			SOCKLIST* msgsock = sockhead;
 			while(msgsock != NULL) {
 				if(FD_ISSET(msgsock->csock,
 							&rfds)) { /* ソケットから受信したなら */
 					printf("\e[1K\r");
-					/* ソケットから読み込み端末に出力 */
 					if((nbytes = read(msgsock->csock, rbuf, sizeof(rbuf)))
 					   < 0) {
 						perror("read");
 					} else if(nbytes == 0) {
+						// s7 (離脱処理)
 						close(msgsock->csock);
 						printf("client %s closed\n", msgsock->username);
+						char closed_name[101];
+						strcpy(closed_name, msgsock->username);
 						remove_sock(msgsock->csock);
+
+						char buf[1024] = "";
+						sprintf(buf, "%s exited\n", closed_name);
+						broadcast(buf);
 						break;
 					} else {
-						char buf[1024];
+						char buf[1024] = "";
 						rbuf[nbytes] = '\0';
-						SOCKLIST *bsock = sockhead;
-						while(bsock != NULL) {
+						if(strcmp(rbuf, "/list\n") == 0) {
+							SOCKLIST* member = sockhead;
+							while(member != NULL) {
+								strcat(buf, member->username);
+								strcat(buf, "\n");
+								member = member->next;
+							}
+							broadcast(buf);
+						} else if(strncmp(rbuf, "/send ", 6) == 0) {
+							char targetname[101], msg[1024];
+							sscanf("/send %s %s", targetname, msg);
+							SOCKLIST* target = sockhead;
+							while(target != NULL) {
+								if(strcmp(target->username, targetname) == 0) {
+									break;
+								}
+								target = target->next;
+							}
+							if(target == NULL) {
+								write(msgsock->csock, "No such user\n", 14);
+							} else {
+								sprintf(buf, "%s*>%s", msgsock->username, msg);
+								write(target->csock, buf, strlen(buf));
+							}
+						} else {
 							sprintf(buf, "%s >%s", msgsock->username, rbuf);
-							write(bsock->csock, buf, strlen(buf));
-							bsock = bsock->next;
+							broadcast(buf);
 						}
+						break;
 					}
 				}
-				msgsock = msgsock->next;
+			}
+			msgsock = msgsock->next;
+		} else {
+			if(timeup) {
+				SOCKLIST* csock = sockhead;
+				while(csock != NULL) {
+					close(csock->csock);
+				}
+				close(sock);
+				exit(0);
+			} else if(errno == EINTR) {
+				alarm(10);
+				signal(SIGALRM, timeup);
+
+				broadcast("Server >10 seconds remaining\n");
 			}
 		}
 	}
